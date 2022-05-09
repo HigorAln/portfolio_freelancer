cea11386402da00285276891e7b501ae825e82b3
Higor Allan <higor.allan21@gmail.com>
2022-05-08 22:53:57 -0300

    retirando overflow-x

--- src/services/work.ts
+++ src/services/work.ts
@@ -1,17 +1,18 @@
 export const WorkVariables = [
+	// {
+	// 	id: 1,
+	// 	title: 'Frontend',
+	// 	url: '/patriani.png',
+	// 	description:
+	// 		'I currently work at Workfy where we are developing the website and platform for the patriani construction company, with incredible dashboards.',
+	//   color: "var(--purple)",
+	// },
 	{
-		id: 1,
-		title: 'Frontend',
-		url: '/patriani.png',
+		id: 2,
+		title: 'Ladding page',
+		url: '/food.jpg',
 		description:
-			'I currently work at Workfy where we are developing the website and platform for the patriani construction company, with incredible dashboards.',
-    color: "var(--purple)",
+			'I created this landing page for a food company. It is a simple landing page with a simple design.',
+		color: 'var(--pink)',
 	},
-  {
-    id: 2,
-    title: 'Ladding page',
-    url: "/food.jpg",
-    description: "I created this landing page for a food company. It is a simple landing page with a simple design.",
-    color: "var(--pink)",
-  }
 ];
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -1,5 +1,3 @@
 import styled from 'styled-components';
 
-export const Content = styled.div`
-  width: 100vw;
-`;
+export const Content = styled.div``;
